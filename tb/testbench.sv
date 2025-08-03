f8ef823bfc238e348aee064ed1cae6cd90490ea7
Ammar-10xE <ammar.sarwar@10xengineers.ai>
2025-08-03 17:51:50 +0500

    updated testbench

--- tb/testbench.sv
+++ tb/testbench.sv
@@ -1,58 +1,41 @@
-///////////////////////////////////////////////////////////////////////////////
-// File:        testbench.sv
-// Author:      Cristian Florin Slav
-// Date:        2023-06-27
-// Description: Testbench module. It contains the instance of the DUT and the 
-//              logic to start the UVM test and UVM phases.
-///////////////////////////////////////////////////////////////////////////////
-`include "cfs_algn_test_pkg.sv"
+// ///////////////////////////////////////////////////////////////////////////////
+// // File:        testbench.sv
+// // Author:      Ammar Khan
+// // Description: Testbench module. It contains the instance of the DUT and the
+// //              logic to start the UVM test and UVM phases.
+// ///////////////////////////////////////////////////////////////////////////////
 
-module testbench();
-  
-  import uvm_pkg::*;
-  import cfs_algn_test_pkg::*;
-  
-  //CLock signal
-  reg clk;
-  
-  //Reset signal - active low
-  reg reset_n;
-  
-  //Clock generator
-  initial begin
+`include "cfs_algn_test_pkg.sv"
+import uvm_pkg::*;
+import cfs_algn_test_pkg::*;
+
+module testbench;
+  reg clk,reset_n;
+
+  initial begin // 100MHZ clk generation
     clk = 0;
-    
-    forever begin
-      //Generate an 100MHz clock
-      clk = #5ns ~clk;
-    end
+    forever #5 clk = ~clk;
   end
-  
-  //Initial reset generator
-  initial begin
+
+  initial begin // reset generation
     reset_n = 1;
-    
-    #6ns;
-    
+    repeat(5) @(posedge clk);
     reset_n = 0;
-    
-    #30ns;
+    repeat(5) @(posedge clk);
     reset_n = 1;
   end
-  
-  initial begin
-    $dumpfile("dump.vcd");
-    $dumpvars;
-    
-    //Start UVM test and phases
-    run_test("");
-  end
-  
+
   //Instantiate the DUT
   cfs_aligner dut(
     .clk(    clk),
     .reset_n(reset_n)
   );
-  
-  
+
+  // Start UVM test and phases
+  initial begin
+    run_test("");
+    $dumpfile("dump.vcd");
+    $dumpvars;
+  end
+
 endmodule
