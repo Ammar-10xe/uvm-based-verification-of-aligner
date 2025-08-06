cc9d00861b963dc4a3d3c743d5614d530fe1866a
Ammar-10xE <ammar.sarwar@10xengineers.ai>
2025-08-06 16:42:59 +0500

    added apb pkg

---
+++ tb/cfs_apb_pkg.sv
@@ -0,0 +1,21 @@
+///////////////////////////////////////////////////////////////////////////////
+// File:        cfs_apb_pkg.sv
+// Author:      Ammar Khan
+// Description: APB Agent package.
+///////////////////////////////////////////////////////////////////////////////
+
+`ifndef CFS_APB_PKG_SV
+  `define CFS_APB_PKG_SV
+
+  `include "uvm_macros.svh"
+  `include "cfs_apb_defines.sv"
+  `include "cfs_apb_if.sv"
+  package cfs_apb_pkg;
+    import uvm_pkg::*;
+
+    // `include "cfs_apb_types.sv"
+    // `include "cfs_apb_agent_config.sv"
+    // `include "cfs_apb_agent.sv"
+  endpackage
+
+`endif
