b4cee0e8a3a893ec5cac4e2466fec967e941875a
Ammar-10xE <ammar.sarwar@10xengineers.ai>
2025-08-06 16:42:28 +0500

    added apb defines

---
+++ tb/cfs_apb_defines.sv
@@ -0,0 +1,13 @@
+///////////////////////////////////////////////////////////////////////////////
+// File:        cfs_apb_defines.sv
+// Author:      Ammar Khan
+// Description: APB Agent package.
+///////////////////////////////////////////////////////////////////////////////
+
+  `ifndef CFS_APB_MAX_DATA_WIDTH
+  	`define CFS_APB_MAX_DATA_WIDTH 32
+  `endif
+
+  `ifndef CFS_APB_MAX_ADDR_WIDTH
+  	`define CFS_APB_MAX_ADDR_WIDTH 32
+  `endif
