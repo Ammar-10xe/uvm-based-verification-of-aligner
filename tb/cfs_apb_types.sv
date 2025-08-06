614091dab7f70d5b5c4106eba2b0071430d07256
Ammar-10xE <ammar.sarwar@10xengineers.ai>
2025-08-06 17:38:18 +0500

    added agent config and types class

---
+++ tb/cfs_apb_types.sv
@@ -0,0 +1,12 @@
+///////////////////////////////////////////////////////////////////////////////
+// File:        cfs_apb_types.sv
+// Author:      Ammar Khan
+// Description: Types used by the APB agent
+///////////////////////////////////////////////////////////////////////////////
+`ifndef CFS_APB_TYPES_SV
+  `define CFS_APB_TYPES_SV
+
+    //Virtual interface type
+    typedef virtual cfs_apb_if cfs_apb_vif;
+
+`endif
